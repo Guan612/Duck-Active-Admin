2ec58c405ec988f0dc8db1cbc858c7017fe8e58e
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-26 11:36:49 +0800

    引入api管理和用户模块

--- sever/src/main.ts
+++ sever/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('鸭鸭活动管理')
+    .setDescription('鸭鸭活动管理API文档')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());// 全局验证
+
   await app.listen(3000);
 }
 bootstrap();
