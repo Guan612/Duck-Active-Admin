fbeebaf00f9d484e6b0372932031938eaf0c451e
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-27 16:49:17 +0800

    调整返回格式

--- sever/src/user/service/auth.service.ts
+++ sever/src/user/service/auth.service.ts
@@ -20,7 +20,8 @@ export class AuthService {
         const payload = { userInfo };
         return {
           message: '登录成功',
-          userInfo: { ...userInfo, token: this.jwtService.sign(payload) },
+          ...userInfo,
+          token: this.jwtService.sign(payload),
         };
       } else {
         throw new HttpException('密码错误', 404);
