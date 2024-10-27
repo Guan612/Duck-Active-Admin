f6f08c89a2747fc01b9739b771ba770c20350cd2
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-27 16:49:36 +0800

    Update app.module.ts

--- sever/src/app.module.ts
+++ sever/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ActiveModule } from './active/active.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ActiveModule],
   controllers: [],
   providers: [],
 })
