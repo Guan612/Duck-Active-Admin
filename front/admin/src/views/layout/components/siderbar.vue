bea9a0ff1b44f3861a149b0a3c10c9da8d5263e1
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-10-28 17:01:42 +0800

    首页布局

---
+++ front/admin/src/views/layout/components/siderbar.vue
@@ -0,0 +1,131 @@
+<script setup lang="ts">
+import { reactive, watch, h } from 'vue';
+import {
+    PieChartOutlined,
+    MailOutlined,
+    DesktopOutlined,
+    InboxOutlined,
+    AppstoreOutlined,
+} from '@ant-design/icons-vue';
+const state = reactive({
+    collapsed: false,
+    selectedKeys: ['1'],
+    openKeys: ['sub1'],
+    preOpenKeys: ['sub1'],
+});
+const items = reactive([
+    {
+        key: '1',
+        icon: () => h(PieChartOutlined),
+        label: 'Option 1',
+        title: 'Option 1',
+    },
+    {
+        key: '2',
+        icon: () => h(DesktopOutlined),
+        label: 'Option 2',
+        title: 'Option 2',
+    },
+    {
+        key: '3',
+        icon: () => h(InboxOutlined),
+        label: 'Option 3',
+        title: 'Option 3',
+    },
+    {
+        key: 'sub1',
+        icon: () => h(MailOutlined),
+        label: 'Navigation One',
+        title: 'Navigation One',
+        children: [
+            {
+                key: '5',
+                label: 'Option 5',
+                title: 'Option 5',
+            },
+            {
+                key: '6',
+                label: 'Option 6',
+                title: 'Option 6',
+            },
+            {
+                key: '7',
+                label: 'Option 7',
+                title: 'Option 7',
+            },
+            {
+                key: '8',
+                label: 'Option 8',
+                title: 'Option 8',
+            },
+        ],
+    },
+    {
+        key: 'sub2',
+        icon: () => h(AppstoreOutlined),
+        label: 'Navigation Two',
+        title: 'Navigation Two',
+        children: [
+            {
+                key: '9',
+                label: 'Option 9',
+                title: 'Option 9',
+            },
+            {
+                key: '10',
+                label: 'Option 10',
+                title: 'Option 10',
+            },
+            {
+                key: 'sub3',
+                label: 'Submenu',
+                title: 'Submenu',
+                children: [
+                    {
+                        key: '11',
+                        label: 'Option 11',
+                        title: 'Option 11',
+                    },
+                    {
+                        key: '12',
+                        label: 'Option 12',
+                        title: 'Option 12',
+                    },
+                    {
+                        key: '13',
+                        label: 'Option 13',
+                        title: 'Option 13',
+                    },
+                    {
+                        key: '14',
+                        label: 'Option 14',
+                        title: 'Option 14',
+                    }
+                ],
+            },
+        ],
+    },
+]);
+watch(
+    () => state.openKeys,
+    (_val, oldVal) => {
+        state.preOpenKeys = oldVal;
+    },
+);
+const toggleCollapsed = () => {
+    state.collapsed = !state.collapsed;
+    state.openKeys = state.collapsed ? [] : state.preOpenKeys;
+};
+</script>
+<template>
+    <div class="flex flex-col">
+        <a-button type="primary" @click="toggleCollapsed">
+            <MenuUnfoldOutlined v-if="state.collapsed" />
+            <MenuFoldOutlined v-else />
+            展开
+        </a-button>
+        <a-menu class="flex-1 overflow-y-auto" v-model:openKeys="state.openKeys"
+            v-model:selectedKeys="state.selectedKeys" mode="inline" :inline-collapsed="state.collapsed"
+            :items="items"></a-menu>
+    </div>
+</template>
---
+++ front/admin/src/views/layout/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Headerbar from './components/headerbar.vue';
+import Siderbar from './components/siderbar.vue';
+
+</script>
+<template>
+    <div class="flex flex-col">
+        <Headerbar></Headerbar>
+        <div class="flex flex-row h-screen">
+            <Siderbar></Siderbar>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
