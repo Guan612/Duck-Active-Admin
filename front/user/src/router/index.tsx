d2453352fd6064cddb8946a43e18be9977ff502f
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-11-06 16:32:07 +0800

    Update index.tsx

--- front/user/src/router/index.tsx
+++ front/user/src/router/index.tsx
@@ -2,11 +2,23 @@ import { useRoutes, Navigate } from "react-router-dom";
 
 import Layout from "../pages/layout";
 import Login from "../pages/login";
+import Home from "../pages/home";
+import Active from "../pages/active";
 
 const routes = [
 	{
 		path: "/",
 		element: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/active",
+				element: <Active />,
+			},
+		],
 	},
 	{
 		path: "/login",
