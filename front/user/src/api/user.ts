903a83907f16bdb0c6815aef70bc50598366c147
Guan612 <tiantony53@163.com>
2024-11-25 11:14:31 +0800

    用户api

---
+++ front/user/src/api/user.ts
@@ -0,0 +1,10 @@
+import http from ".";
+import { loginDto } from "../dto/userDto";
+
+export const registerAPI = (data:loginDto)=>{
+    return http.post("/user/register", data)
+}
+
+export const loginAPI = (data:loginDto)=>{
+    return http.post("/user/login", data)
+}
