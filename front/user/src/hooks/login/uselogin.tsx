fb4ddd269cc5e6e59c685793aca7bdf4bc4db187
Guan612 <tiantony53@163.com>
2024-11-25 11:16:33 +0800

    完成登陆hooks

--- front/user/src/hooks/login/uselogin.tsx
+++ front/user/src/hooks/login/uselogin.tsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { loginAPI } from "../../api/user";
+import userStore from "../../stores/userstore";
+import { message } from "antd";
 import { loginDto } from "../../dto/userDto";
 
-export function useLogin() {
-	const onFinish = (values: loginDto) => {
-		console.log("Success:", values);
+const useLogin = () => {
+	const navigate = useNavigate(); //使用跳转函数
+	const { setUserInfo } = userStore();
+	const onFinish = async (values: loginDto) => {
+		try {
+			const { userInfo } = await loginAPI(values);
+			setUserInfo(userInfo);
+			navigate("/");
+			message.success("欢迎你，" + userInfo.loginId);
+		} catch (error) {
+			console.error("Login failed:", error);
+		}
+	};
+	const onFinishFailed = (errorInfo) => {
+		console.log("Failed:", errorInfo);
 	};
 
-	return { onFinish };
-}
+	return { onFinish, onFinishFailed };
+};
+
+export default useLogin; // 导出useLogin函数
