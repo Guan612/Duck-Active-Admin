f8af9031267413d1323c685d8af7f131e97939cc
Guan612 <tiantony53@163.com>
2024-11-25 11:16:20 +0800

    新增注册dto

--- front/user/src/dto/userDto.ts
+++ front/user/src/dto/userDto.ts
@@ -2,3 +2,11 @@ export interface loginDto {
     loginId: string;
     password: string;
 }
+
+export interface registerDto {
+    loginId: string;
+    password: string;
+    nickname?: string;
+    email?: string;
+    // role: string;
+}
