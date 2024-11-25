4ebbfe66dace87a7a5bea438d53d34a18efd4541
Guan612 <tiantony53@163.com>
2024-11-25 11:16:43 +0800

    首页修改

--- front/user/src/pages/home/index.tsx
+++ front/user/src/pages/home/index.tsx
@@ -2,9 +2,17 @@ import { Carousel } from "antd";
 export default function Home() {
 	return (
 		<div className="flex flex-col justify-center">
-			<div className="min-w-96 h-36 m-2">
-				<Carousel autoplay className="h-full">
-					<div className="bg-teal-300">123</div>
+			<div className="m-2">
+				<Carousel autoplay className="">
+					<div className="bg-teal-300 h-60 rounded-xl">
+						<img
+							src="https://n.sinaimg.cn/sinakd20119/600/w1920h1080/20230425/5579-5ae8a7f932ff9d2be5ddce7225c5c5dc.jpg"
+							alt=""
+							className="rounded-xl"
+						/>
+						<div className="">123</div>
+					</div>
+						
 					<div className="bg-teal-400">456</div>
 				</Carousel>
 			</div>
