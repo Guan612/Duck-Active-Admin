eb22fa0b34f98501afbebbcf02a10a356fc0c45d
Guan612 <tiantony53@163.com>
2024-11-26 19:28:03 +0800

    解决前端基础布局

--- front/admin/components.d.ts
+++ front/admin/components.d.ts
@@ -7,13 +7,21 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
+    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
     AButton: typeof import('ant-design-vue/es')['Button']
     ACheckbox: typeof import('ant-design-vue/es')['Checkbox']
     AForm: typeof import('ant-design-vue/es')['Form']
     AFormItem: typeof import('ant-design-vue/es')['FormItem']
     AInput: typeof import('ant-design-vue/es')['Input']
     AInputPassword: typeof import('ant-design-vue/es')['InputPassword']
+    ALayout: typeof import('ant-design-vue/es')['Layout']
+    ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
+    ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
+    ALayoutSider: typeof import('ant-design-vue/es')['LayoutSider']
     AMenu: typeof import('ant-design-vue/es')['Menu']
+    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
+    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
     IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
--- front/admin/src/views/layout/components/headerbar.vue
+++ front/admin/src/views/layout/components/headerbar.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="flex flex-row bg-gradient-to-r from-transblue via-white to-transpink items-center h-1/10">
+    <a-layout-header class="flex flex-row bg-gradient-to-r from-transblue via-white to-transpink items-center h-1/10">
         <div class="font-bold text-2xl p-2">鸭鸭活动管理</div>
-    </div>
+    </a-layout-header>
 </template>
