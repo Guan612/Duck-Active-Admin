71e69aa832942838317101e4465a22d823ca960e
Guan612 <tiantony53@163.com>
2024-11-27 16:56:20 +0800

    调整前端布局

--- front/admin/src/views/layout/components/headerbar.vue
+++ front/admin/src/views/layout/components/headerbar.vue
@@ -3,6 +3,6 @@
 
 <template>
     <a-layout-header class="flex flex-row bg-gradient-to-r from-transblue via-white to-transpink items-center h-1/10">
-        <div class="font-bold text-2xl p-2">鸭鸭活动管理</div>
+        <div class="font-bold text-2xl p-2"></div>
     </a-layout-header>
 </template>
--- front/admin/src/views/layout/components/siderbar.vue
+++ front/admin/src/views/layout/components/siderbar.vue
@@ -3,8 +3,9 @@
 </script>
 <template>
     <div class="flex flex-col bg-transblue">
-        <div>123</div>
-        <div>123</div>
-        <div>123</div>
+        <div class="flex flex-col justify-center items-center">
+            <img src="../../../assets/logo.svg" class="w-20 h-20" alt="">
+            <p class="text-white text-xl font-bold">鸭鸭活动管理</p>
+        </div>
     </div>
 </template>
