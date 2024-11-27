7ecb6f4b53805b5419b1a1363906880f7d67f2de
Guan612 <tiantony53@163.com>
2024-11-27 18:41:10 +0800

    添加存储

--- front/admin/src/main.ts
+++ front/admin/src/main.ts
@@ -1,16 +1,19 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import "tailwindcss/tailwind.css";
 
 import App from "./App.vue";
 import router from "./router";
 
-
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 const app = createApp(App);
 
-app.use(createPinia());
+app.use(pinia);
+
 app.use(router);
 
 app.mount("#app");
--- front/admin/src/stores/userinfo.ts
+++ front/admin/src/stores/userinfo.ts
@@ -1,9 +1,15 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { LoginAPI } from '@/api/user'
+import type { loginDto } from '@/dto/userDto'
 
 export const useUserInfo = defineStore('user', 
   () => {
     const userInfo = ref({})
+    const getUserInfo = async(data:loginDto)=>{
+      const res = await LoginAPI(data)
+      userInfo.value = res.userInfo
+    }
 
     return {  }
 })
