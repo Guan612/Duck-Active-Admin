de21fac1718bd5692716ceb1d487493f2ad0ab75
Guan612 <tiantony53@163.com>
2024-11-29 16:49:54 +0800

    完成拦截器

--- front/admin/src/api/index.ts
+++ front/admin/src/api/index.ts
@@ -1,14 +1,37 @@
 import axios from "axios";
-import { message } from 'ant-design-vue';
+import { useUserStore } from "@/stores/userstore";
+import { message } from "ant-design-vue";
+import router from "@/router";
 const http = axios.create({
-	baseURL: "http://localhost:3000",
-	timeout: 2000,
+  baseURL: "http://localhost:3000",
+  timeout: 2000,
 });
 
 http.interceptors.request.use(
-	
+  async (config) => {
+    const userStore = useUserStore();
+    const token = await userStore.userInfo?.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    message.error(error.message);
+    return Promise.reject(error);
+  }
 );
 
-http.interceptors.response.use();
+http.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    console.log(error);
+    message.error(error.response.data.message || '未知错误');
+    return Promise.reject(error);
+  }
+);
 
 export default http;
