62a570f61a1245c7a09dc149603c724b9b9746a5
Guan612 <tiantony53@163.com>
2024-11-29 09:38:49 +0800

    完成本地存储

--- front/admin/src/stores/userinfo.ts
+++ front/admin/src/stores/userinfo.ts
@@ -1,15 +1,27 @@
 import { ref, computed } from 'vue'
+import { message } from 'ant-design-vue';
 import { defineStore } from 'pinia'
 import { LoginAPI } from '@/api/user'
 import type { loginDto } from '@/dto/userDto'
+import router from '@/router';
 
 export const useUserInfo = defineStore('user', 
   () => {
     const userInfo = ref({})
     const getUserInfo = async(data:loginDto)=>{
       const res = await LoginAPI(data)
-      userInfo.value = res.userInfo
+      userInfo.value = res.userInfo;
+      if(res){
+        message.success('欢迎你'+res.userInfo.loginId);
+        router.push('/')
+      }
     }
 
-    return {  }
+    const logOut = ()=>{
+      userInfo.value = {};
+      message.success('退出登录成功');
+      router.push('/auth/login')
+    }
+
+    return { userInfo,getUserInfo,logOut }
 })
