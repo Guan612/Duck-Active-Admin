23231c3af448e0e43f32cbf0a8fad25bf674d35a
Guan612 <tiantony53@163.com>
2024-11-30 19:18:31 +0800

    前端页面构建

--- front/user/src/pages/active/index.tsx
+++ front/user/src/pages/active/index.tsx
@@ -1,8 +1,48 @@
+import { Select } from "antd";
+
 export default function Active() {
+	const options = [];
+	for (let i = 10; i < 36; i++) {
+	options.push({
+		label: i.toString(36) + i,
+		value: i.toString(36) + i,
+	});
+	}
+	const handleChange = (value) => {
+	console.log(`selected ${value}`);
+	};
 	return (
 		<div className="flex flex-col items-center justify-center h-full">
-			<h1 className="text-4xl font-bold mb-4">Active Page</h1>
-			<p className="text-lg">This is the active page</p>
+			<div>
+				<h1 className="text-2xl font-bold mb-4">活动选择</h1>
+				<Select
+					className="w-64 mx-2"
+					mode="multiple"
+					allowClear
+					placeholder="选择活动"
+					defaultValue={['a10', 'c12']}
+					onChange={handleChange}
+					options={options}
+				/>
+				<Select
+					className="w-64 mx-2"
+					mode="multiple"
+					allowClear
+					placeholder="选择类型"
+					defaultValue={['a10', 'c12']}
+					onChange={handleChange}
+					options={options}
+				/>
+				<Select
+					className="w-64 mx-2"
+					mode="multiple"
+					allowClear
+					placeholder="选择时间"
+					defaultValue={['a10', 'c12']}
+					onChange={handleChange}
+					options={options}
+				/>
+			</div>
 		</div>
 	);
 }
--- front/user/src/pages/home/index.tsx
+++ front/user/src/pages/home/index.tsx
@@ -4,16 +4,23 @@ export default function Home() {
 		<div className="flex flex-col justify-center">
 			<div className="m-2">
 				<Carousel autoplay className="">
-					<div className="bg-teal-300 h-60 rounded-xl">
+					<div className="flex flex-row bg-teal-300 rounded-xl">
 						<img
 							src="https://n.sinaimg.cn/sinakd20119/600/w1920h1080/20230425/5579-5ae8a7f932ff9d2be5ddce7225c5c5dc.jpg"
 							alt=""
-							className="rounded-xl"
+							className="rounded-xl h-52 justify-center"
 						/>
-						<div className="">123</div>
+						<div className="mx-2 text-xl font-bold">123</div>
 					</div>
 						
-					<div className="bg-teal-400">456</div>
+					<div className="bg-teal-400 rounded-xl">
+						<img
+							src="https://n.sinaimg.cn/sinakd20119/600/w1920h1080/20230425/5579-5ae8a7f932ff9d2be5ddce7225c5c5dc.jpg"
+							alt=""
+							className="rounded-xl h-52 items-center"
+						/>
+						<div className="mx-2 text-xl font-bold">456</div>
+					</div>
 				</Carousel>
 			</div>
 			<div className="flex flex-row justify-around">
