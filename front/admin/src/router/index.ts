ae53fd30e81670055856e1f7a9051922c53084be
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-12-03 16:49:50 +0800

    添加前端token验证

--- front/admin/src/router/index.ts
+++ front/admin/src/router/index.ts
@@ -4,6 +4,8 @@ import Home from "@/views/home/index.vue";
 import Auth from "@/views/auth/index.vue";
 import Login from "@/views/auth/login/index.vue";
 import Register from "@/views/auth/register/index.vue";
+import { message } from "ant-design-vue";
+import { useUserStore } from "@/stores/userstore";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -42,8 +44,15 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  //token部分
-  next();
-})
+	//token部分
+	const userStore = useUserStore();
+	if (to.meta.requiresAuth && !userStore.userInfo?.token) {
+		message.error("请先登录");
+		next("/auth/login");
+	} else {
+		next();
+	}
+	next();
+});
 
 export default router;
--- front/user/src/router/index.tsx
+++ front/user/src/router/index.tsx
@@ -1,10 +1,24 @@
 import { useRoutes, Navigate } from "react-router-dom";
 
+import useAuth from "../hooks/login/useauth";
+import { message } from "antd";
+
 import Layout from "../pages/layout";
 import Login from "../pages/login";
 import Home from "../pages/home";
 import Active from "../pages/active";
 import Me from "../pages/me";
+
+const RequireAuth = ({ element }) => {
+	const auth = useAuth();
+	if (!auth) {
+		message.error("请先登录后使用");
+		return <Navigate to="/login" />;
+	} else {
+		return element;
+	}
+	//return auth ? element : <Navigate to="/auth" />;
+};
 
 const routes = [
 	{
@@ -17,11 +31,11 @@ const routes = [
 			},
 			{
 				path: "/active",
-				element: <Active />,
+				element: <RequireAuth element={<Active />} />,
 			},
 			{
 				path: "/me",
-				element: <Me />,
+				element: <RequireAuth element={<Me />} />,
 			},
 		],
 	},
