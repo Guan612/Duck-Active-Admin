3c6adbe8c3e394c6105094aa31024a8113b8583c
Guan612 <tiantony53@163.com>
2024-12-05 11:08:04 +0800

    新增路由跳转逻辑

--- front/user/src/hooks/active/useactivecard.tsx
+++ front/user/src/hooks/active/useactivecard.tsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
 export default function useActiveCard() {
+	const navigate = useNavigate();
+	
 	const cardItems = Array(20).fill(null);
+
+	const goActiveDetail = (id: string) => {
+		console.log(id)
+	    navigate(`/activedetail/${id}`)
+	}
 
 	return {
 		cardItems,
+		goActiveDetail,
 	};
 }
