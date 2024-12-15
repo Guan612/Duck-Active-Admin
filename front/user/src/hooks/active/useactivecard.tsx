afdfb3cf9acd205fe79292cef7fc519314459a7b
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-12-15 17:32:38 +0800

    调整获取所有活动hooks位置

--- front/user/src/hooks/active/useactivecard.tsx
+++ front/user/src/hooks/active/useactivecard.tsx
@@ -1,31 +1,17 @@
-import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getActiveListAPI } from "../../api/active";
+import { ActivitieType } from "../../dto/activeDto";
 export default function useActiveCard() {
-	const [cardItems, setCardItems] = useState([]);
-
 	const navigate = useNavigate();
 
-	const getActiveList = async () => {
-		const res = await getActiveListAPI();
-		setCardItems(res);
-	};
-
 	const getActivitieType = (typeValue: number): string => {
-		return menuType[typeValue] || "未知活动类型";
+		return ActivitieType[typeValue] || "未知活动类型";
 	};
 
-	const goActiveDetail = (id: string) => {
-		console.log(id);
+	const goActiveDetail = (id: number) => {
 		navigate(`/activedetail/${id}`);
 	};
 
-	useEffect(() => {
-		getActiveList()
-	}, []);
-
 	return {
-		cardItems,
 		goActiveDetail,
 		getActivitieType,
 	};
