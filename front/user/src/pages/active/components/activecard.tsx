fdae3e604f58be4f695015234fd00ab99becfcab
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-12-15 17:34:33 +0800

    卡片改为动态渲染

--- front/user/src/pages/active/components/activecard.tsx
+++ front/user/src/pages/active/components/activecard.tsx
@@ -1,10 +1,14 @@
+import dayjs from "dayjs";
 import { Button, Image } from "antd";
 import useActiveCard from "../../../hooks/active/useactivecard";
-export default function ActiveCard({ cardInfo }) {
-	const {goActiveDetail} = useActiveCard()
+import { ActiveDto, ActivitieType } from "../../../dto/activeDto";
+export default function ActiveCard({ cardInfo }: { cardInfo: ActiveDto }) {
+	const { goActiveDetail } = useActiveCard();
 	return (
 		<div className="flex flex-col m-2">
-			<div className="text-xl font-bold text-center">{cardInfo.title}</div>
+			<div className="text-xl font-bold text-center">
+				{cardInfo.title}
+			</div>
 			<div className="">
 				<img
 					className="rounded-lg"
@@ -13,11 +17,22 @@ export default function ActiveCard({ cardInfo }) {
 			</div>
 			<div className="flex justify-between items-center">
 				<div>
-					<div className="">类型：{}</div>
+					<div className="">
+						类型：
+						{ActivitieType[cardInfo.activitieType] || "未知类型"}
+					</div>
 					<div className="">地点：{cardInfo.activeAddress}</div>
-					<div className="">开始时间：{cardInfo.startDate}</div>
+					<div className="">
+						开始时间：
+						{dayjs(cardInfo.startDate).format("YYYY-MM-DD")}
+					</div>
 				</div>
-				<Button className="font-bold" onClick={() => goActiveDetail(cardInfo.id)}>详情</Button>
+				<Button
+					className="font-bold"
+					onClick={() => goActiveDetail(cardInfo.id)}
+				>
+					详情
+				</Button>
 			</div>
 		</div>
 	);
