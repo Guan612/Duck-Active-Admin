447a066f6281bb026557c8d7dbc0ce17c4a4cd9c
Guan612 <tiantony53@163.com>
2024-12-17 11:39:07 +0800

    为前端渲染活动信息做好准备

--- front/user/src/api/registration.ts
+++ front/user/src/api/registration.ts
@@ -4,6 +4,10 @@ export function isJoinAPI(id: string) {
 	return http.get("/registration/isJoin", { params: { activeId: id } });
 }
 
+export function getMyActiveAPI() {
+	return http.get("/registration/myactive");
+}
+
 export function addActiveAPI(data: object) {
 	return http.post("/registration/join", data);
 }
--- front/user/src/hooks/me/useuserinfo.tsx
+++ front/user/src/hooks/me/useuserinfo.tsx
@@ -1,9 +1,23 @@
 import userStore from "../../stores/userstore";
-
+import { getMyActiveAPI } from "../../api/registration";
+import { useEffect, useState } from "react";
+import { ActiveDto } from "../../dto/activeDto";
 export default function useUserInfo() {
+	const[myActive,setMyActive] = useState<ActiveDto>([])
+
 	const userInfo = userStore.getState().userInfo;
 
+	const getMyActive = async () => {
+		const res = await getMyActiveAPI();
+		setMyActive(res)
+	}
+
+	useEffect(() => {
+	    getMyActive()
+	},[])
+
 	return {
+		myActive,
 		userInfo,
 	};
 }
