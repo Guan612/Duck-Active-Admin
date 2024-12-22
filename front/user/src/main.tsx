62eb3916626164b6bddf3259f907e2d8bf30d6ef
Guan612 <tiantony53@163.com>
2024-12-22 20:36:29 +0800

    引入dayjs

--- front/user/src/main.tsx
+++ front/user/src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+dayjs.locale('zh-cn');
 import zhCN from 'antd/locale/zh_CN';
 import './index.css'
 import App from './App.tsx'
