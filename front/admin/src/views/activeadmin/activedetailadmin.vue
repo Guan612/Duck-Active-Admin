575e598b72ad96180eabed731a14c379e28cf0fe
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-12-26 15:08:11 +0800

    完成审核活动

--- front/admin/src/views/activeadmin/activedetailadmin.vue
+++ front/admin/src/views/activeadmin/activedetailadmin.vue
@@ -2,7 +2,10 @@
 import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router';
 import { LeftOutlined } from '@ant-design/icons-vue';
-import { getActiveDetailAPI } from '@/api/active';
+import dayjs from 'dayjs';
+import { getActiveDetailAPI, updateActiveAPI } from '@/api/active';
+import { ActivitieType } from '@/dto/activeDto';
+import { message } from 'ant-design-vue';
 
 const router = useRouter();
 const activedetail = ref({})
@@ -11,6 +14,26 @@ const paramsid = router.currentRoute.value.params.id;
 const getActivedetail = async (id: string) => {
     const res = await getActiveDetailAPI(id);
     activedetail.value = res;
+}
+
+const getActiveType = (type: number) => {
+    return ActivitieType[type] ?? '未知类型';
+}
+
+const pass = async (id: number) => {
+    const res = await updateActiveAPI(id, { activitStatus: 2 });
+    if (res) {
+        message.success('审核通过');
+        router.push('/activereviewadmin')
+    }
+}
+
+const reject = async (id: number) => {
+    const res = await updateActiveAPI(id, { activitStatus: 6 });
+    if (res) {
+        message.success('审核拒绝');
+        router.push('/activereviewadmin')
+    }
 }
 
 onMounted(() => {
@@ -33,9 +56,21 @@ onMounted(() => {
                 <img :src="activedetail.activitieImgUrl" alt="活动图片" class="m-2 rounded-lg object-cover" />
             </div>
             <div class="flex flex-col md:w-1/2">
-                <div>{{ activedetail.title }}</div>
+                <div class="text-xl font-bold m-2">活动名称：{{ activedetail.title }}</div>
+                <div class="text-lg m-2">活动发起人：{{ activedetail.nickname || activedetail.loginId }}</div>
+                <div class="text-lg m-2">活动类型：{{ getActiveType(activedetail.activitieType) }}</div>
+                <div class="text-lg m-2">
+                    活动时间：{{ dayjs(activedetail.startDateDate).format("YYYY-MM-DD") }} - {{
+                        dayjs(activedetail.endDate).format("YYYY-MM-DD") }}
+                </div>
+                <div class="text-lg m-2">活动地点：{{ activedetail.activeAddress }}</div>
+                <div class="text-lg m-2">活动人数：{{ activedetail.activitiePeopleNum }}</div>
+                <div class="text-lg m-2">活动详情：{{ activedetail.content }}</div>
+                <div class="flex justify-end">
+                    <a-button type='primary' class="mx-2" @click="pass(activedetail.id)">通过审核</a-button>
+                    <a-button type="primary" danger @click="reject(activedetail.id)">拒绝通过</a-button>
+                </div>
             </div>
         </div>
-
     </div>
 </template>
