bd0034c228a7b4f1ab509260b1a1dc0d4f7e23a5
Guan612 <71303298+Guan612@users.noreply.github.com>
2024-12-26 11:11:07 +0800

    新增查询活动状态api

--- front/admin/src/api/active.ts
+++ front/admin/src/api/active.ts
@@ -9,12 +9,24 @@ export function getActiveListAPI() {
 	return http.get("/active");
 }
 
+export function getActiveDetailAPI(id: string) {
+	return http.get(`/active/${id}`);
+}
+
 export function getActiveByCreatUser() {
-    return http.get('/active/findByCreatUser');
+	return http.get("/active/findByCreatUser");
+}
+
+export function getActiveStatusAPI(statusArray: number[]) {
+	return http.get("/active/findByStatus", {
+		params: {
+			status: statusArray.join(","), // 将数组转换为逗号分隔的字符串
+		},
+	});
 }
 
 export function updateActiveAPI(id: number, data: updateActiveDto) {
-    return http.patch(`/active/${id}`, data);
+	return http.patch(`/active/${id}`, data);
 }
 
 export function deleteActiveAPI(id: number) {
