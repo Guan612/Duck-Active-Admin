bf6932a0d3b9df629b90eb7816e43da83afa1247
Guan612 <tiantony53@163.com>
2024-12-30 09:00:51 +0800

    移植管理端界面

--- front/admin/src/views/activeadmin/responsibleadmin.vue
+++ front/admin/src/views/activeadmin/responsibleadmin.vue
@@ -1,6 +1,52 @@
 <script setup lang="ts">
+import { getActiveByCreatUser } from '@/api/active';
+import { ActivitieStatus } from '@/dto/activeDto';
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
+const activeList = ref([]);
+const router = useRouter();
+
+const getActiveList = async () => {
+    const res = await getActiveByCreatUser();
+    activeList.value = res;
+}
+
+//申请按钮
+const requestReview = async (id: number) => {
+    const res = await updateActiveAPI(id, { activitStatus: 1 });
+    if (res) {
+        message.success('发起申请成功');
+        getActiveList();
+    }
+}
+
+const getActivitieStatusText = (status: number): string => {
+    return ActivitieStatus[status] ?? '未知状态';
+};
+
+onMounted(() => {
+    getActiveList();
+})
 </script>
 <template>
-    <div>学生管理端</div>
+    <div class="flex flex-col">
+        <div v-for="item in activeList" :key="item.id"
+                class="flex flex-col md:flex-row justify-between items-center bg-transblue rounded-lg m-2 p-2">
+                <div>
+                    <div class="font-bold text-xl">{{ item.title }}</div>
+                    <div>活动状态：{{ getActivitieStatusText(item.activitStatus) }}</div>
+                </div>
+
+                <div>
+                    <a-button @click="requestReview(item.id)"
+                        :disabled="!(item.activitStatus == 0 || item.activitStatus == 6)" class="mx-2">
+                        发起审核申请
+                    </a-button>
+                    <a-button @click=""
+                        :disabled="!(item.activitStatus == 2 || item.activitStatus == 3)">开启报名</a-button>
+                </div>
+
+            </div>
+    </div>
 </template>
--- front/admin/src/views/activeadmin/teacheradmin.vue
+++ front/admin/src/views/activeadmin/teacheradmin.vue
@@ -1,5 +1,49 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import { updateActiveAPI, getActiveByCreatUser, getActiveStatusAPI } from '@/api/active';
+import { ActivitieStatus } from '@/dto/activeDto';
+import { message } from 'ant-design-vue';
+import dayjs from 'dayjs';
+import { useRouter } from 'vue-router';
+
+const activeList = ref([]);
+const router = useRouter();
+
+const getActiveList = async () => {
+    const res = await getActiveStatusAPI([1]);
+    activeList.value = res
+}
+
+//活动状态匹配
+const getActivitieStatusText = (status: number): string => {
+    return ActivitieStatus[status] ?? '未知状态';
+};
+
+//快速更改活动状态
+const qucikPass = async (id: number) => {
+    const res = await updateActiveAPI(id, { activitStatus: 2 });
+    if (res) {
+        message.success('审核通过');
+        getActiveList();
+    }
+}
+
+onMounted(() => {
+    getActiveList();
+})
 </script>
 <template>
-    <div>老师管理界面</div>
+    <div class='flex flex-col'>
+        <div v-for="item in activeList" :key="item.id"
+                class="flex flex-col md:flex-row justify-between items-center bg-transblue rounded-lg m-2 p-2">
+            <div class="flex flex-col">
+                <div>{{ item.title }}</div>
+                <div>开始时间：{{ dayjs(item.startDate).format("YYYY-MM-DD") }}</div>
+            </div>
+             <div class="flex">
+                <a-button @click="router.push(`/activedetailadmin/${item.id}`)" class="mx-2">详情</a-button>
+                <a-button @click="qucikPass(item.id)">快速通过</a-button>
+            </div>
+        </div>
+    </div>
 </template>
