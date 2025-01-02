71e30869e36e3cf61eb58ddcbcc8d8b2f9640c87
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-01-02 10:50:10 +0800

    首页hooks

--- front/user/src/hooks/home/usehome.tsx
+++ front/user/src/hooks/home/usehome.tsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+import { getNewActiveAPI } from "../../api/active";
+
 export default function useHome() {
-    
+    const [hotactivities, setHotActivities] = useState([]);
+    const [newactivities, setNewActivities] = useState([]);
+
+    const getNewActivities = async () => {
+        const res = await getNewActiveAPI()
+        setNewActivities(res)
+    }
+
+    useEffect(() => {
+        getNewActivities()
+    },[])
+
+    return{
+        newactivities,
+    }
 }
