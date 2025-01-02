7c31f4ef1b62b87fb4fd2200adfbc2e18a33868f
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-01-02 10:50:01 +0800

    调整首页显示

--- front/user/src/pages/home/index.tsx
+++ front/user/src/pages/home/index.tsx
@@ -1,40 +1,41 @@
 import { Carousel } from "antd";
+import useHome from "../../hooks/home/usehome";
+import { ActiveDto } from "../../dto/activeDto";
 export default function Home() {
+	const { newactivities } = useHome();
 	return (
 		<div className="flex flex-col justify-center">
 			<div className="m-2">
-				<Carousel autoplay className="">
-					<div className="flex flex-row bg-teal-300 rounded-xl">
-						<img
-							src="https://n.sinaimg.cn/sinakd20119/600/w1920h1080/20230425/5579-5ae8a7f932ff9d2be5ddce7225c5c5dc.jpg"
-							alt=""
-							className="rounded-xl h-52 justify-center"
-						/>
-						<div className="mx-2 text-xl font-bold">123</div>
-					</div>
-
-					<div className="bg-teal-400 rounded-xl">
-						<img
-							src="https://n.sinaimg.cn/sinakd20119/600/w1920h1080/20230425/5579-5ae8a7f932ff9d2be5ddce7225c5c5dc.jpg"
-							alt=""
-							className="rounded-xl h-52 items-center"
-						/>
-						<div className="mx-2 text-xl font-bold">456</div>
-					</div>
+				<Carousel autoplay className="flex justify-center">
+					{newactivities.map((item: ActiveDto) => (
+						<div className="md:w-1/3 h-36 flex flex-col bg-transblue rounded-lg">
+							<div className="font-bold text-xl m-1 text-center">
+								{item.title}
+							</div>
+							<img
+								src={item.activitieImgUrl}
+								alt=""
+								className="h-20 rounded-lg"
+							></img>
+						</div>
+					))}
 				</Carousel>
 			</div>
 			<div className="flex flex-col justify-around m-2">
 				<div className="text-xl font-bold text-center">热门活动</div>
-				<div className="flex flex-col md:flex-row">
-					<div className="md:w-1/3 h-36 rounded-lg m-1 bg-teal-500">
-						123
-					</div>
-					<div className="md:w-1/3 h-36 rounded-lg m-1 bg-teal-500">
-						123
-					</div>
-					<div className="md:w-1/3 h-36 rounded-lg m-1 bg-teal-500">
-						123
-					</div>
+				<div className="flex flex-col md:flex-row justify-center">
+					{newactivities.map((item: ActiveDto) => (
+						<div className="md:w-1/3 h-36 flex flex-col bg-transblue rounded-lg m-1 p-1 hover:hover:shadow-xl">
+							<div className="font-bold text-xl m-1 text-center">
+								{item.title}
+							</div>
+							<img
+								src={item.activitieImgUrl}
+								alt=""
+								className="h-20 rounded-lg"
+							></img>
+						</div>
+					))}
 				</div>
 
 				<div className="text-xl font-bold text-center">热门资讯</div>
