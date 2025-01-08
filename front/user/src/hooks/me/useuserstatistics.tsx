c550a1dcc45c4f27ada31161a9f46c71b985877a
Guan612 <tiantony53@163.com>
2025-01-08 12:37:06 +0800

    对接活动动态数据

--- front/user/src/hooks/me/useuserstatistics.tsx
+++ front/user/src/hooks/me/useuserstatistics.tsx
@@ -1,39 +1,53 @@
+import { useEffect, useState } from "react";
+import { getMyActiveAPI } from "../../api/registration";
+
 export default function useUserStatistics() {
-    const options = {
-		title: {
-			text: "得分统计",
-		},
-		tooltip: {},
-		xAxis: {
-			data: ["明德", "笃行", "尽美", "博学"],
-		},
-		yAxis: {},
-		series: [
-			{
-				name: "分数",
-				type: "bar",
-				data: [5, 10, 5, 10],
-			},
-		],
-	};
+  const [userData, setUserData] = useState([]);
+  const combinedStats = {}; //参加月份数据
+  const getUserData = async () => {
+    const res = await getMyActiveAPI();
+    setUserData(res);
+  };
 
-    const options2 = {
-        title:{
-            text:'参加次数统计'
-        },
-        tooltip: {},
-        xAxis: {
-            data:['九月', '十月', '十一月', '十二月'],
-        },
-        yAxis: {},
-        series:[
-            {
-                name:'参加次数',
-                type:'bar',
-                data:[1, 3, 2, 4]
-            }
-        ]
-    }
+  const options = {
+    title: {
+      text: "得分统计",
+    },
+    tooltip: {},
+    xAxis: {
+      data: ["明德", "笃行", "尽美", "博学"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "分数",
+        type: "bar",
+        data: [5, 10, 5, 10],
+      },
+    ],
+  };
 
-    return { options, options2 };
+  const options2 = {
+    title: {
+      text: "参加次数统计",
+    },
+    tooltip: {},
+    xAxis: {
+      data: ["九月", "十月", "十一月", "十二月"],
+    },
+    yAxis: {},
+    series: [
+      {
+        name: "参加次数",
+        type: "bar",
+        data: [1, 3, 2, 4],
+      },
+    ],
+  };
+
+  useEffect(() => {
+    getMyActiveAPI();
+  }, []);
+
+  return { options, options2 };
 }
