8717c45d689588c478d1d719d50bf159d1880705
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-01-09 11:47:38 +0800

    调整当没有热门的显示

--- front/user/src/pages/home/index.tsx
+++ front/user/src/pages/home/index.tsx
@@ -24,18 +24,24 @@ export default function Home() {
 			<div className="flex flex-col justify-around m-2">
 				<div className="text-xl font-bold text-center">热门活动</div>
 				<div className="flex flex-col md:flex-row justify-center">
-					{newactivities.map((item: ActiveDto) => (
-						<div className="md:w-1/3 h-36 flex flex-col bg-transblue rounded-lg m-1 p-1 hover:hover:shadow-xl">
-							<div className="font-bold text-xl m-1 text-center">
-								{item.title}
+					{newactivities.length > 0 ? (
+						newactivities.map((item: ActiveDto) => (
+							<div className="md:w-1/3 h-36 flex flex-col bg-transblue rounded-lg m-1 p-1 hover:hover:shadow-xl">
+								<div className="font-bold text-xl m-1 text-center">
+									{item.title}
+								</div>
+								<img
+									src={item.activitieImgUrl}
+									alt=""
+									className="h-20 rounded-lg"
+								></img>
 							</div>
-							<img
-								src={item.activitieImgUrl}
-								alt=""
-								className="h-20 rounded-lg"
-							></img>
+						))
+					) : (
+						<div className="text-center text-gray-500">
+							暂无热门活动
 						</div>
-					))}
+					)}
 				</div>
 
 				<div className="text-xl font-bold text-center">热门资讯</div>
