9db455fa9b953807af6d6d16405cc272d2588c79
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-03-04 15:23:23 +0800

    新用户端基础框架搭建

---
+++ frontV2/user/src/router/index.tsx
@@ -0,0 +1,26 @@
+import { useRoutes, Navigate } from "react-router";
+
+import Layout from "../pages/layout";
+import Home from "../pages/home";
+import Me from "../pages/me";
+
+const routes = [
+	{
+		path: "/",
+		elemnet: <Layout />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/me",
+				element: <Me />,
+			},
+		],
+	},
+];
+
+export default function Router() {
+	return useRoutes(routes);
+}
