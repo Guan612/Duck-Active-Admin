0788963c0ba3503fd8d5f53f617b6af498ab0d4d
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-03-05 16:36:49 +0800

    添加admin的daisyui

--- frontV2/admin/vite.config.ts
+++ frontV2/admin/vite.config.ts
@@ -17,4 +17,10 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+		host: "0.0.0.0",
+		port: 6121,
+		// 是否开启 https
+		https: false,
+	},
 })
