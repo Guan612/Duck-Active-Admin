e261818af0ddda477eb0e18dc187bc1a0d0e829e
Guan612 <71303298+Guan612@users.noreply.github.com>
2025-03-10 15:42:40 +0800

    添加了全局提示

--- frontV2/user/src/App.tsx
+++ frontV2/user/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router";
 import Router from "./router";
+import { Toaster } from "react-hot-toast";
 
 function App() {
 	return (
 		<BrowserRouter>
+			<Toaster position="top-center" />
 			<Router />
 		</BrowserRouter>
 	);
