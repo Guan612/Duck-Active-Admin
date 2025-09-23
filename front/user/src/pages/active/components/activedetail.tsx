ae8a8f36bba2acd15710b953c96cee0c0223e76e
Guan612 <tiantony53@163.com>
2025-09-23 19:00:12 +0800

    预添加活动live框

--- front/user/src/pages/active/components/activedetail.tsx
+++ front/user/src/pages/active/components/activedetail.tsx
@@ -1,14 +1,22 @@
 import dayjs from "dayjs";
-import { LeftOutlined, CheckCircleOutlined, EnvironmentOutlined, CalendarOutlined, TagOutlined } from "@ant-design/icons";
+import {
+  LeftOutlined,
+  CheckCircleOutlined,
+  EnvironmentOutlined,
+  CalendarOutlined,
+  TagOutlined,
+} from "@ant-design/icons";
 import { Button, Card, Tag, Typography, Grid, Image, Space } from "antd";
 import useActiveDetail from "../../../hooks/active/useactivedetail";
 import { ActivitieStatus, ActivitieType } from "../../../dto/activeDto";
+import LiveChat from "./livechat";
 
 const { Title, Text } = Typography;
 const { useBreakpoint } = Grid;
 
 export default function ActiveDetail() {
-  const { backActive, activeDetail, joinActive, joinStatus } = useActiveDetail();
+  const { backActive, activeDetail, joinActive, joinStatus } =
+    useActiveDetail();
   const screens = useBreakpoint();
 
   const statusColorMap = {
@@ -17,34 +25,34 @@ export default function ActiveDetail() {
     6: "gray", // 活动取消
     2: "green",
     3: "red",
-    1: "blue"
+    1: "blue",
   };
   return (
     <div className="p-4 md:p-8 bg-gray-50 min-h-screen">
       <Card className="rounded-2xl shadow-xs mb-6">
         <Button
-					icon={<LeftOutlined />}
+          icon={<LeftOutlined />}
           onClick={backActive}
           className="mb-4 flex items-center"
           type="text"
-				>
+        >
           返回活动列表
-				</Button>
+        </Button>
         <div className="flex flex-col lg:flex-row gap-8">
           <div className="lg:w-1/2">
             <Image
-						src={activeDetail.activitieImgUrl}
+              src={activeDetail.activitieImgUrl}
               className="rounded-xl object-cover"
               height={screens.md ? 400 : 240}
               preview={false}
             />
-				</div>
+          </div>
           <div className="flex-1">
             <Space className="mb-4" align="start">
               <Tag
-                color={statusColorMap[activeDetail.activitStatus] || 'blue'}
+                color={statusColorMap[activeDetail.activitStatus] || "blue"}
                 className="text-sm px-3 py-1 rounded-full"
-						>
+              >
                 {ActivitieStatus[activeDetail.activitStatus] || "未知状态"}
               </Tag>
               <Title level={3} className="m-0">
@@ -60,7 +68,7 @@ export default function ActiveDetail() {
                   <Tag color="geekblue" className="ml-2">
                     {ActivitieType[activeDetail.activitieType] || "未知类型"}
                   </Tag>
-					</div>
+                </div>
 
                 <div className="flex items-center">
                   <CalendarOutlined className="text-lg mr-2 text-orange-500" />
@@ -69,7 +77,7 @@ export default function ActiveDetail() {
                     {dayjs(activeDetail.startDate).format("YYYY/MM/DD HH:mm")} -
                     {dayjs(activeDetail.endDate).format("YYYY/MM/DD HH:mm")}
                   </Text>
-				</div>
+                </div>
 
                 <div className="flex items-center">
                   <EnvironmentOutlined className="text-lg mr-2 text-red-500" />
@@ -77,17 +85,19 @@ export default function ActiveDetail() {
                   <Text className="ml-2" ellipsis>
                     {activeDetail.activeAddress}
                   </Text>
-			</div>
+                </div>
 
                 <div className="flex items-center">
                   <CheckCircleOutlined className="text-lg mr-2 text-green-500" />
                   <Text strong>人数：</Text>
                   <Text className="ml-2">
-                    <span className="text-blue-600">{activeDetail.remainingNum}</span>
+                    <span className="text-blue-600">
+                      {activeDetail.remainingNum}
+                    </span>
                     <span className="mx-1">/</span>
                     <span>{activeDetail.activitiePeopleNum}</span>
                   </Text>
-		</div>
+                </div>
               </div>
 
               <Card
@@ -116,6 +126,15 @@ export default function ActiveDetail() {
           </div>
         </div>
       </Card>
+      <div className="mt-2">
+        {activeDetail.activitStatus == 4 ? (
+          <LiveChat />
+        ) : (
+          <div className="text-center mt-1 font-bold">
+            活动中可以在这里看同学们的实时聊天哦
+          </div>
+        )}
+      </div>
     </div>
-	);
+  );
 }
---
+++ front/user/src/pages/active/components/livechat.tsx
@@ -0,0 +1,9 @@
+import { Card } from "antd";
+
+export default function LiveChat() {
+  return (
+    <Card>
+      <div>123123</div>
+    </Card>
+  );
+}
