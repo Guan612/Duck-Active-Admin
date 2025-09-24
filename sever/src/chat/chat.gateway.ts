cc29fe54d5672d7a072cc364e179842b21b1f986
Guan612 <tiantony53@163.com>
2025-09-24 17:13:46 +0800

    完成聊天订阅消息

--- sever/src/chat/chat.gateway.ts
+++ sever/src/chat/chat.gateway.ts
@@ -6,23 +6,25 @@ import {
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { JwtAuthGuard } from 'src/user/guard/jwt.guard';
+import { ChatService } from './chat.service';
+import { createChatContexDto } from './dto/chat.dto';
 
-@WebSocketGateway(3001, {
-  namespace: '/chat',
-  cors: {
-    origin: '*', // 允许所有来源
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-  },
-})
+@WebSocketGateway(3001, { namespace: '/chat', cors: { origin: '*' } })
 @UseGuards(JwtAuthGuard)
 export class ChatGateway {
+  constructor(private chatService: ChatService) {}
   @WebSocketServer()
   server: Server;
 
   @SubscribeMessage('intoChat')
   handleMessage(client: any, payload: any): string {
     return 'Hello world!';
+  }
+
+  @SubscribeMessage('sendContext')
+  async HandleMessage(client: any, payload: createChatContexDto) {
+    const res = await this.chatService.crate(payload);
+    return res;
   }
 
   @SubscribeMessage('exitChat')
